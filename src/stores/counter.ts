f7e834d795eef968657423156a5cf5aad2b8a614
lice <1435646450lice@gmail.com>
2022-02-17 11:21:32 +0800

    初始化

---
+++ src/stores/counter.ts
@@ -0,0 +1,14 @@
+export const useCounterStore = defineStore({
+  id: "counter",
+  state: () => ({
+    counter: 0,
+  }),
+  getters: {
+    doubleCount: (state) => state.counter * 2,
+  },
+  actions: {
+    increment() {
+      this.counter++;
+    },
+  },
+});
---
+++ vite.config.ts
@@ -0,0 +1,70 @@
+import { fileURLToPath, URL } from 'url';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import OptimizationPersist from 'vite-plugin-optimize-persist';
+import PkgConfig from 'vite-plugin-package-config';
+import Icons from 'unplugin-icons/vite';
+import IconsResolver from 'unplugin-icons/resolver';
+import { FileSystemIconLoader } from 'unplugin-icons/loaders';
+
+export default defineConfig({
+	plugins: [
+		vue(),
+		AutoImport({
+			// 这里除了引入 vue 以外还可以引入pinia、vue-router、vueuse等，
+			// 甚至你还可以使用自定义的配置规则，见 https://github.com/antfu/unplugin-auto-import#configuration
+			imports: ['vue', 'vue-router', 'pinia'],
+			// 第三方组件库的解析器
+			resolvers: [
+				ElementPlusResolver({
+					importStyle: 'sass',
+				}),
+			],
+			dts: './types/auto-imports.d.ts',
+			eslintrc: {
+				enabled: true, // Default `false`
+				filepath: './eslint/.eslintrc-auto-import.json',
+				globalsPropValue: true,
+			},
+		}),
+		Components({
+			dirs: ['src/components/'],
+			extensions: ['vue', 'md'],
+			resolvers: [
+				ElementPlusResolver({
+					importStyle: 'sass',
+				}),
+				IconsResolver({
+					customCollections: ['custom'],
+				}),
+			],
+			dts: './types/components.d.ts',
+		}),
+		Icons({
+			compiler: 'vue3',
+			customCollections: {
+				custom: FileSystemIconLoader('./src/assets/icons'),
+			},
+		}),
+		PkgConfig(),
+		OptimizationPersist(),
+	],
+	server: {
+		host: '0.0.0.0',
+	},
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: `@use "@/styles/element/index.scss" as *;`,
+			},
+		},
+	},
+});
